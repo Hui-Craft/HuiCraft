42639474677b563b57ee054dc2473d22cdb388d1
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-26 16:52:59 +0000

    Change load order

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -6,8 +6,8 @@ hui._internal = {}
 hui.api = {}
 
 --Logging
-hui._internal.log("main", "Logging works!")
 dofile(minetest.get_modpath("main") .. "/log.lua")
+hui._internal.log("main", "Logging works!")
 
 --Load files
 dofile(minetest.get_modpath("main") .. "/loader.lua")
