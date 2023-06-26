2df5ee10f4db4cad0783536972c1233842d35b55
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-26 19:23:54 +0000

    Player hand

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -12,3 +12,18 @@ hui._internal.log("main", "Logging works!")
 --Load files
 dofile(minetest.get_modpath("main") .. "/loader.lua")
 hui._internal.loadfile("main", "mapgen/init")
+
+--Hand
+minetest.override_item("", {
+	wield_scale = {x=1,y=1,z=2.5},
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
+		},
+		damage_groups = {fleshy=1},
+	}
+})
