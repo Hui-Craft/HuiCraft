15d0041d2a384c3b72e74ffb07f35683d4995649
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-26 17:15:04 +0000

    Change some flags

--- mods/blocks/blocks/stone.lua
+++ mods/blocks/blocks/stone.lua
@@ -3,6 +3,6 @@ hui._internal.log("blocks", "Registering stone")
 minetest.register_node("blocks:stone", {
     description = "Stone",
     tiles = {"blocks_stone.png"},
-    is_ground_content = true,
+    is_ground_content = false,
     groups = {cracky=3, stone=1}
 })
