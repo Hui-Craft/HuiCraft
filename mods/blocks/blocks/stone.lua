e71b38a7d0768878b1338026df56ba41317b7961
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-26 17:21:01 +0000

    Block flags

--- mods/blocks/blocks/stone.lua
+++ mods/blocks/blocks/stone.lua
@@ -4,5 +4,5 @@ minetest.register_node("blocks:stone", {
     description = "Stone",
     tiles = {"blocks_stone.png"},
     is_ground_content = false,
-    groups = {cracky=3, stone=1}
+    groups = {stone=1}
 })
