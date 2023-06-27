7e0e97a24b47945ffefd56b8ff9a132b691cffb2
CodingRule <91148727+CodingRule@users.noreply.github.com>
2023-06-27 20:49:58 +0300

    Update oak_leaf.lua

--- mods/blocks/blocks/oak_leaf.lua
+++ mods/blocks/blocks/oak_leaf.lua
@@ -1,7 +1,7 @@
-hui._internal.log("blocks", "Registering leaf")
+hui._internal.log("blocks", "Registering oak_leaf")
 
-minetest.register_node("blocks:leaf", {
-    description = "Leaf",
+minetest.register_node("blocks:oak_leaf", {
+    description = "Oak Leaf",
     tiles = {"blocks_oak_leaf.png"},
     is_ground_content = true,
     groups = {crumbly=1, cracky=0}
