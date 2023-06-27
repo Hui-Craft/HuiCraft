3561e79f76cb16aec0d8a764e5b3af39523d93fc
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:02:40 +0000

    Block breaking

--- mods/blocks/blocks/grass.lua
+++ mods/blocks/blocks/grass.lua
@@ -4,5 +4,5 @@ minetest.register_node("blocks:grass", {
     description = "Grass block",
     tiles = {"blocks_grass.png"},
     is_ground_content = true,
-    groups = {crumbly=1, cracky=0}
+    groups = {crumbly=1, oddly_breakable_by_hand=3}
 })
--- mods/blocks/blocks/stone.lua
+++ mods/blocks/blocks/stone.lua
@@ -4,5 +4,5 @@ minetest.register_node("blocks:stone", {
     description = "Stone",
     tiles = {"blocks_stone.png"},
     is_ground_content = false,
-    groups = {stone=1, cracky=3}
+    groups = {stone=1, cracky=1, oddly_breakable_by_hand=1}
 })
