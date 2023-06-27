4acafea3b6c6530fe5fc17e54d4842d23b262cf3
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:28:33 +0000

    GO back to png

--- mods/blocks/blocks/oak_plank.lua
+++ mods/blocks/blocks/oak_plank.lua
@@ -2,7 +2,7 @@ hui._internal.log("blocks", "Registering oak_plank")
 
 minetest.register_node("blocks:oak_plank", {
     description = "Oak Plank",
-    tiles = {"blocks_oak_planks.jpg"},
+    tiles = {"blocks_oak_planks.png"},
     is_ground_content = true,
     groups = {choppy=1}
 })
