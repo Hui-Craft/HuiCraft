a4f70ecfe21d226c5259f57bdfec063fdeb401ab
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:22:11 +0000

    Stone

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -20,12 +20,12 @@ minetest.override_item("", {
 		full_punch_interval = 1,
 		max_drop_level = 50,
 		groupcaps = {
-			stone = {times={[1]=10.00}, uses=50, maxlevel=1000},
 			crumbly = {times={[1]=1.00}, uses=50, maxlevel=1000},
+			cracky = {times={[1]=10.00}, uses=50, maxlevel=1000},
 			snappy = {times={[1]=0.50}, uses=50, maxlevel=1000},
 			oddly_breakable_by_hand = {times={[1]=1.00,[2]=2.00,[3]=3.00}, uses=50, maxlevel=1000},
 			choppy = {times={[1]=5.00}, uses=50, maxlevel=1000}
 		},
-		damage_groups = {fleshy=1, snappy=1, crumbly=1, stone=1, oddly_breakable_by_hand=1, choppy=1},
+		damage_groups = {fleshy=1, snappy=1, crumbly=1, oddly_breakable_by_hand=1, choppy=1, cracky=1},
 	}
 })
