bab34a2cc3a1c5ad0d2e5a341e21127cd89304e6
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:05:14 +0000

    Break times

--- mods/blocks/blocks/dirt.lua
+++ mods/blocks/blocks/dirt.lua
@@ -4,5 +4,5 @@ minetest.register_node("blocks:dirt", {
     description = "Dirt",
     tiles = {"blocks_dirt.png"},
     is_ground_content = true,
-    groups = {crumbly=1, oddly_breakable_by_hand=3}
+    groups = {crumbly=1, oddly_breakable_by_hand=2}
 })
