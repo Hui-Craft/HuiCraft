7a28ce80c99f131a201307a199546cd045303415
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:55:15 +0000

    Remove water

--- mods/main/mapgen/essentials.lua
+++ mods/main/mapgen/essentials.lua
@@ -1,7 +1,7 @@
 minetest.register_alias("mapgen_dirt", "blocks:dirt")
-minetest.register_alias("mapgen_dirt_with_grass", "blocks:dirt")
+minetest.register_alias("mapgen_dirt_with_grass", "blocks:grass")
 
 minetest.register_alias("mapgen_stone", "blocks:stone")
 
-minetest.register_alias("mapgen_water_source", "blocks:dirt")
-minetest.register_alias("mapgen_river_water_source", "blocks:dirt")
+-- minetest.register_alias("mapgen_water_source", "blocks:dirt")
+-- minetest.register_alias("mapgen_river_water_source", "blocks:dirt")
