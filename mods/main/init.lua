228c4f758e56628d8117eadaac26c9044371dc84
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:31:49 +0000

    Yes

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -20,11 +20,11 @@ minetest.override_item("", {
 		full_punch_interval = 1,
 		max_drop_level = 50,
 		groupcaps = {
-			crumbly = {times={[1]=1.00}, uses=1, maxlevel=1000},
-			cracky = {times={[1]=10.00}, uses=1, maxlevel=1000},
-			snappy = {times={[1]=0.50}, uses=1, maxlevel=1000},
-			oddly_breakable_by_hand = {times={[1]=1.00}, uses=1, maxlevel=1000},
-			choppy = {times={[1]=5.00}, uses=1, maxlevel=1000}
+			crumbly = {times={[1]=1.00}, uses=1, maxlevel=10},
+			cracky = {times={[1]=10.00}, uses=1, maxlevel=10},
+			snappy = {times={[1]=0.50}, uses=1, maxlevel=10},
+			oddly_breakable_by_hand = {times={[1]=1.00}, uses=1, maxlevel=10},
+			choppy = {times={[1]=5.00}, uses=1, maxlevel=10}
 		},
 		damage_groups = {fleshy=1, snappy=1, crumbly=1, oddly_breakable_by_hand=1, choppy=1, cracky=1},
 	}
