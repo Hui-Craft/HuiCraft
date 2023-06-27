af7a60046519344115a72dd8e5bafc72cc3ff90c
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 17:59:19 +0000

    Fix typo

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -20,7 +20,7 @@ minetest.override_item("", {
 		full_punch_interval = 5,
 		max_drop_level = 0,
 		groupcaps = {
-			stone = {times={[2]=3.00, [3]=0.70}, uses=1, maxlevel=3}
+			stone = {times={[2]=3.00, [3]=0.70}, uses=1, maxlevel=3},
 			crumbly = {times={[2]=3.00, [3]=0.70}, uses=1, maxlevel=1},
 			snappy = {times={[3]=0.40}, uses=1, maxlevel=1},
 			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=5}
