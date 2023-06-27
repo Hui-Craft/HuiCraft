47b33ad38a1231e016d4f2254dbbdb7487336ecc
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:30:23 +0000

    Uses

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -17,14 +17,14 @@ hui._internal.loadfile("main", "mapgen/init")
 minetest.override_item("", {
 	wield_scale = {x=1,y=1,z=2.5},
 	tool_capabilities = {
-		full_punch_interval = 10,
+		full_punch_interval = 1,
 		max_drop_level = 50,
 		groupcaps = {
-			crumbly = {times={[1]=1.00}, uses=50, maxlevel=1000},
-			cracky = {times={[1]=10.00}, uses=50, maxlevel=1000},
-			snappy = {times={[1]=0.50}, uses=50, maxlevel=1000},
-			oddly_breakable_by_hand = {times={[1]=1.00}, uses=50, maxlevel=1000},
-			choppy = {times={[1]=5.00}, uses=50, maxlevel=1000}
+			crumbly = {times={[1]=1.00}, uses=1, maxlevel=1000},
+			cracky = {times={[1]=10.00}, uses=1, maxlevel=1000},
+			snappy = {times={[1]=0.50}, uses=1, maxlevel=1000},
+			oddly_breakable_by_hand = {times={[1]=1.00}, uses=1, maxlevel=1000},
+			choppy = {times={[1]=5.00}, uses=1, maxlevel=1000}
 		},
 		damage_groups = {fleshy=1, snappy=1, crumbly=1, oddly_breakable_by_hand=1, choppy=1, cracky=1},
 	}
