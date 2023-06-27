a26f0a529b1a361795dd993ccbf25c5846694aeb
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-27 18:22:53 +0000

    JPG file

--- mods/blocks/blocks/oak_plank.lua
+++ mods/blocks/blocks/oak_plank.lua
@@ -2,7 +2,7 @@ hui._internal.log("blocks", "Registering oak_plank")
 
 minetest.register_node("blocks:oak_plank", {
     description = "Oak Plank",
-    tiles = {"blocks_oak_planks.png"},
+    tiles = {"blocks_oak_planks.jpg"},
     is_ground_content = true,
     groups = {choppy=1}
 })
