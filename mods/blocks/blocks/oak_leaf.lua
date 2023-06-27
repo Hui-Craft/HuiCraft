a9d756555c5ff10d31424a1cd004b9ab7d359137
CodingRule <91148727+CodingRule@users.noreply.github.com>
2023-06-27 20:48:53 +0300

    Update and rename leaf.lua to oak_leaf.lua

--- mods/blocks/blocks/oak_leaf.lua
+++ mods/blocks/blocks/oak_leaf.lua
@@ -2,7 +2,7 @@ hui._internal.log("blocks", "Registering leaf")
 
 minetest.register_node("blocks:leaf", {
     description = "Leaf",
-    tiles = {"blocks_leaf.png"},
+    tiles = {"blocks_oak_leaf.png"},
     is_ground_content = true,
     groups = {crumbly=1, cracky=0}
 })
