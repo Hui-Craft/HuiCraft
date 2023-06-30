e1f536e5e976bf33f9f9c122337d8b499619d5b7
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-06-30 15:34:31 +0000

    Stick name

--- mods/items/items/stick.lua
+++ mods/items/items/stick.lua
@@ -1,12 +1,12 @@
-minetest.register_craftitem("item:stick", {
+minetest.register_craftitem("items:stick", {
     description = "Stick",
     inventory_image = "item_sticks.png"
 })
-minetest.register_alias("stick", "item:stick")
+minetest.register_alias("stick", "items:stick")
 
 minetest.register_craft({
     type = "shaped",
-    output = "item:stick",
+    output = "items:stick",
     recipe = {
         {"blocks:oak_plank"},
         {"blocks:oak_plank"}
